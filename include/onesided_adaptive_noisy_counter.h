3b816844ec004cede7f9ec751375f2ba9796293e
高木駿 <tkgsn@h18.169.229.10.1018236.vlan.kuins.net>
2020-11-27 17:53:16 +0900

    onesided adaptive noisy counter header

---
+++ include/onesided_adaptive_noisy_counter.h
@@ -0,0 +1,22 @@
+#ifndef ONESIDED_INCLUDED_ADAPTIVE_NOISY_COUNTER
+#define ONESIDED_INCLUDED_ADAPTIVE_NOISY_COUNTER
+
+#include <math.h>
+#include "adaptive_noisy_counter.h"
+#include "util.h"
+using namespace std;
+
+class OnesidedAdaptiveNoisyCounter : public AdaptiveNoisyCounter{
+public:
+    OnesidedAdaptiveNoisyCounter(float epsilon_query, int k, float ratio_of_bigger_epsilon=0.5, int seed=0): AdaptiveNoisyCounter(epsilon_query, k){
+        this->bigger_epsilon = epsilon_query * ratio_of_bigger_epsilon;
+        this->epsilon_query = epsilon_query;
+        this->bigger_laplace_query = make_onesided_laplace(this->bigger_epsilon, k);
+        this->laplace_query = make_onesided_laplace(this->epsilon_query, k);
+        this->laplace_threshold = make_zero_noise();
+    };
+    int random_threshold() override;
+    map<int, float> measure(map<int, float>, float) override;
+};
+
+#endif
