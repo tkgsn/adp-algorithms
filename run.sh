5ca3cc19534eb2203c6bfd23d6caec9c1041690b
shun <shun.0721@hotmail.co.jp>
2022-09-06 13:34:07 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,38 +1,38 @@
-# python run_low_sens.py --alpha 0
-# python run_low_sens.py --alpha 0.1
-# python run_low_sens.py --alpha 0.01
-# python run_low_sens.py --alpha 0.001
-# python run_low_sens.py --alpha 0.0001
+python run_low_sens.py --alpha 0
+python run_low_sens.py --alpha 0.1
+python run_low_sens.py --alpha 0.01
+python run_low_sens.py --alpha 0.001
+python run_low_sens.py --alpha 0.0001
 
-# python run_low_sens.py --epsilon 0.1
-# python run_low_sens.py --epsilon 0.5
-# python run_low_sens.py --epsilon 1
-# python run_low_sens.py --epsilon 2
-# python run_low_sens.py --epsilon 5
+python run_low_sens.py --epsilon 0.1
+python run_low_sens.py --epsilon 0.5
+python run_low_sens.py --epsilon 1
+python run_low_sens.py --epsilon 2
+python run_low_sens.py --epsilon 5
 
-# python run_low_sens.py --threshold 1
-# python run_low_sens.py --threshold 2
-# python run_low_sens.py --threshold 3
-# python run_low_sens.py --threshold 5
-# python run_low_sens.py --threshold 10
+python run_low_sens.py --threshold 1
+python run_low_sens.py --threshold 2
+python run_low_sens.py --threshold 3
+python run_low_sens.py --threshold 5
+python run_low_sens.py --threshold 10
 
-# python run_low_sens.py --delta 0.1
-# python run_low_sens.py --delta 0.01
-# python run_low_sens.py --delta 0.001
-# python run_low_sens.py --delta 0.0001
-# python run_low_sens.py --delta 0.00001
+python run_low_sens.py --delta 0.1
+python run_low_sens.py --delta 0.01
+python run_low_sens.py --delta 0.001
+python run_low_sens.py --delta 0.0001
+python run_low_sens.py --delta 0.00001
 
-# python run_low_sens.py --alpha 0 --geq
-# python run_low_sens.py --alpha 0.1 --geq
-# python run_low_sens.py --alpha 0.01 --geq
-# python run_low_sens.py --alpha 0.001 --geq
-# python run_low_sens.py --alpha 0.0001 --geq
+python run_low_sens.py --alpha 0 --geq
+python run_low_sens.py --alpha 0.1 --geq
+python run_low_sens.py --alpha 0.01 --geq
+python run_low_sens.py --alpha 0.001 --geq
+python run_low_sens.py --alpha 0.0001 --geq
 
-# python run_high_sens_nquery_beta.py --c 1
-# python run_high_sens_nquery_beta.py --c 2
-# python run_high_sens_nquery_beta.py --c 3
-# python run_high_sens_nquery_beta.py --c 4
-# python run_high_sens_nquery_beta.py --c 5
+python run_high_sens_nquery_beta.py --c 1
+python run_high_sens_nquery_beta.py --c 2
+python run_high_sens_nquery_beta.py --c 3
+python run_high_sens_nquery_beta.py --c 4
+python run_high_sens_nquery_beta.py --c 5
 
 python run_high_sens_nquery_beta.py --epsilon 0.1
 python run_high_sens_nquery_beta.py --epsilon 0.5
